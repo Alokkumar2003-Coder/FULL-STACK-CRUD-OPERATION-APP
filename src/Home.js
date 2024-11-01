795f0bc95ca736475b573ef2343f2f95f76c9fb2
Alokkumar2003-Coder <alokkumar2000.5.5@gmail.com>
2024-11-01 19:33:23 +0530

    Second Commit

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="text-center flex justify-center mt-32">
-      <h2>Welcome to the CRUD OPERATION</h2>
+      <h2>Welcome to the CRUD OPERATION </h2>
     </div>
   );
 };
